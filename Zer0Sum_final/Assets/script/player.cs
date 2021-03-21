f8d9cc5b69c7161733bd97ceb1d38201a3427c7c
yash-deokate <deokateyash@gmail.com>
2021-03-21 09:28:05 +0530

    added Knockback and documented player script

--- Zer0Sum_final/Assets/script/player.cs
+++ Zer0Sum_final/Assets/script/player.cs
@@ -5,27 +5,54 @@ using UnityEngine.UI;
 
 public class player : MonoBehaviour
 {
+    #region variables
+
+    //var for rigidBody
     private Rigidbody2D rigidbody;
-    float horizontalValue;
+     // var for animations
+    Animator animator;
+
+
+    // vars for run and walk
+    public float horizontalValue;
     public float speed=1;
     public bool faceright=true;
     bool isRunning=false;
      float runSpeed = 1.5f;
-    Animator animator;
 
+
+
+    //vars for jumps
     [SerializeField] Transform groundcheckObject;
     private float groundcheckRadius=0.04f;
     public bool isgrounded=false;
-
     [SerializeField] LayerMask groundLayer;
     bool isJumping;
     [SerializeField] public float jumpPower;
 
-    [SerializeField] public Image health;
 
-   private float healthamnt=10f;
+
+
+    // var for health, healthBAr and respawn 
+    [SerializeField] public Image health;
+    private float healthamnt=10f;
     [SerializeField] Transform respawnPoint;
 
+
+
+    // vars for knock back
+    [SerializeField] float knockBackLength=0.5f;
+    [SerializeField] float knockBackForce=15f;
+    bool isHurt=false;
+
+    public bool canMove=true;
+
+   public float thrust;
+
+    #endregion
+   
+   
+   // Runs once when object is created
     void Awake(){
 
         //referencing rigidbody of player
@@ -37,6 +64,7 @@ public class player : MonoBehaviour
  
   
     // Update is called once per frame
+    // using this for taking inputs
     void Update()
     {
         // left right input
@@ -59,12 +87,19 @@ public class player : MonoBehaviour
             isJumping=false;
         }
         
+
     }
+
+    // runs per frame
+    // using this to update physics
     void FixedUpdate(){ 
         groundcheck();
-        Move(horizontalValue,isJumping);
-       
+        if(canMove){
+             Move(horizontalValue,isJumping);
+        }
     }
+    
+    //check if object is in air to avoid jumping when in air
     void groundcheck(){  
            isgrounded=false;
             Collider2D[] collider=Physics2D.OverlapCircleAll(groundcheckObject.position,groundcheckRadius,groundLayer);
@@ -75,7 +110,7 @@ public class player : MonoBehaviour
     }
 
 
-
+   // jump and moving mechanism
     void Move(float dir,bool jump){
 
     #region jump
@@ -127,16 +162,53 @@ public class player : MonoBehaviour
     }
 
 
-
+    // collision with enemy and other opps
     void OnCollisionEnter2D(Collision2D col){
         if(col.gameObject.tag.Equals("enemy")){
+            // hurt animation
+            animator.SetBool("hit",true);
+            //healthbar update
             healthamnt-=1;
             health.fillAmount=healthamnt/10;
+            // knckback on enemy attack
+            doKnockBack(transform.localScale.x);
+
+            //on dying respawn and health bar update
             if(healthamnt==0){
                 transform.position=respawnPoint.position;
                 healthamnt=10;
                 health.fillAmount=healthamnt/10;
             }
         }
+        
     }
+   
+   // restoring animation after enemy attack
+    void OnCollisionExit2D(Collision2D col){
+        if(col.gameObject.tag.Equals("enemy")){
+             animator.SetBool("hit",false);
+        }
+    }
+
+
+    // knock back on enemy attack in given direction
+    public void doKnockBack(float dir){
+        StartCoroutine(DisablePlayerMovement(knockBackLength));
+        rigidbody.velocity=new Vector2(-dir*knockBackForce,knockBackForce);
+    }
+
+    // coroutine to stop moving for given time so that we can knockback player
+    IEnumerator DisablePlayerMovement(float time){
+        // we allow move function(in Fixedupdate) only if canMove is true
+         canMove=false;
+         // there is no use of this var.. just for future purposes
+        isHurt=true;
+        // this animatio change does not make any sense...just trial 
+         animator.SetBool("hit",true);
+        yield return new WaitForSeconds(time);
+         animator.SetBool("hit",false);
+        canMove=true;
+        isHurt=false;
+    }
+   
 }
